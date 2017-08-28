00f34c4a00ff2456856e6de52dab600dd889d37e
tunip <tunip@users.noreply.github.com>
2017-08-28 19:43:05 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 set -e
 
+# Set UID/GID
+PUID=${PUID:-911}
+PGID=${PGID:-911}
+
+groupmod -o -g "$PGID" abc
+usermod -o -u "$PUID" abc
+chown abc:abc /config
+
 exec nginx -c /config/nginx.conf -g "daemon off;"
